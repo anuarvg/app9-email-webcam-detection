1b24696fd039ba656d267be06f36fe4a7bee0966
Anuar <anuar.vasquez@tcpip.tech>
2025-06-27 15:11:12 -0600

    Se agrega captura de imagen y envio por email

--- emailing.py
+++ emailing.py
@@ -1,2 +1,29 @@
-def send_email():
+import smtplib
+import ssl
+from email.message import EmailMessage
+import imghdr
+
+SENDER = "anuar.vasquez@tcpip.tech"
+PASSWORD = "odirevfxjaidjsaf"
+
+host = "smtp.gmail.com"
+port = 465
+def send_email(image_path):
+    context = ssl.create_default_context()
+    receiver = "anuarvg@gmail.com"
+    email_message = EmailMessage()
+    email_message["Subject"] = "Llego nuevo usuario"
+    email_message.set_content("Hey, nosotros vimos un nuevo cliente")
+
+    with open(image_path, "rb") as image:
+        content = image.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(SENDER, PASSWORD)
+        server.sendmail(SENDER,receiver, email_message.as_string())
+
     print("Email was sent!")
+
+# if __name__ == "__main__":
+#     send_email(image_path="images/10.png")
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import glob
+import os
 import time
 import cv2
+from threading import Thread
 
 from emailing import send_email
 
@@ -8,6 +11,13 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+def clean_floder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -34,12 +44,24 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0),3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_floder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
+
     print(status_list)
 
     cv2.imshow("Video", frame)
@@ -50,5 +72,6 @@ while True:
 
 video.release()
 
+clean_thread.start()
 
 
